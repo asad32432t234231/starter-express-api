11aca566f064e6f5fa2f938bbaa3cf3027205fb8
asad32432t234231 <124665090+asad32432t234231@users.noreply.github.com>
2023-02-10 05:02:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+
+app.use(cors());
+
+app.get('/downloadmp4', async (req, res, next) => {
+    try {
+        let url = req.query.url;
+        if (!ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+        let title = 'video';
+
+        await ytdl.getBasicInfo(url, {
+            format: 'mp4'
+        }, (err, info) => {
+            title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+        });
+
+        res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+        ytdl(url, {
+            format: 'mp4',
+        }).pipe(res);
+
+    } catch (err) {
+        console.error(err);
+    }
+});
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
